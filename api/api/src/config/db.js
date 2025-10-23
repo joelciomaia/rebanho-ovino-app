b32d123bf3c8064cf3be0250729f4cc6a6be32e5
Joelcio <joelciomaia@users.noreply.github.com>
2025-10-23 12:24:17 -0300

    conectando as variaveis do banco ao repositório online

--- api/api/src/config/db.js
+++ api/api/src/config/db.js
@@ -2,10 +2,12 @@ const mysql = require('mysql2/promise');
 require('dotenv').config();
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST || 'localhost',
-  user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASS || 'root123',
-  database: process.env.DB_NAME || 'ovinos_banco',
+  host: process.env.DB_HOST || 'mysql-1472b4e9-joelciomaia-ovino.f.aivencloud.com',
+  port: process.env.DB_PORT || 21658,
+  user: process.env.DB_USER || 'avnadmin',
+  password: process.env.DB_PASS || 'AVNS_n9nWd4pJ4DoJxZinQ_V',
+  database: process.env.DB_NAME || 'defaultdb',
+  ssl: process.env.DB_HOST ? { rejectUnauthorized: true } : false,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
